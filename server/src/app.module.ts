670f97a7c0d88a3e0fb17296f66a1d0bdf622fb7
Jye <shokoste1@gmail.com>
2026-08-06 23:28:28 +0700

    set up rate limit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -20,6 +22,16 @@ import { CopilotModule } from './modules/copilot/copilot.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: Number(config.get<number>('THROTTLE_TTL', 60000)),
+          limit: Number(config.get<number>('THROTTLE_LIMIT', 10)),
+        },
+      ],
+    }),
     DatabaseModule,
     CommonServicesModule,
     AiTasksModule,
@@ -35,6 +47,12 @@ import { CopilotModule } from './modules/copilot/copilot.module';
   ],
 
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
